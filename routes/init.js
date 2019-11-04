9a53646b5a4fa737e502f5423470da4f2fa4009a
nigellenl <nigelle.nl@gmail.com>
2019-11-05 04:22:11 +0800

    Admin homepage and other admin pages!

--- routes/init.js
+++ routes/init.js
@@ -69,13 +69,13 @@ app.post('/login', function(req, res, next) {
 			res.redirect('/relog')
 		} else {
 			if (result.rows[0].password == [password]) {
-				pool.query('SELECT "aid" FROM Administrator WHERE "aid" = $1', [uid], (err, result1) => {
+				pool.query('SELECT "aid" FROM Administrators WHERE "aid" = $1', [uid], (err, result1) => {
 					sess = req.body;
 					sess["uid"] = uid;
 					if (result1.rows[0] == null) {
 						res.redirect('/about')
 					} else {
-						res.redirect('/index')
+						res.redirect('/admin_homepage')
 					}
 				});
 			} else {
@@ -83,6 +83,10 @@ app.post('/login', function(req, res, next) {
 			}
 		}
 	});
+});
+// GET for Admin
+app.get('/admin_homepage', function(req, res, next) {
+	res.render('admin_homepage', { title: 'Admin Homepage' });
 });
 
 // Get for About
@@ -114,13 +118,13 @@ app.post('/', function(req, res, next) {
 			res.redirect('/relog')
 		} else {
 			if (result.rows[0].password == [password]) {
-				pool.query('SELECT "aid" FROM Administrator WHERE "aid" = $1', [uid], (err, result1) => {
+				pool.query('SELECT "aid" FROM Administrators WHERE "aid" = $1', [uid], (err, result1) => {
 					sess = req.body;
 					sess.uid = uid;
 					if (result1.rows[0] == null) {
 						res.redirect('/about')
 					} else {
-						res.redirect('/index')
+						res.redirect('/admin_homepage')
 					}
 				});
 			} else {
@@ -129,6 +133,114 @@ app.post('/', function(req, res, next) {
 		}
 	});
 });
+
+// GET for Course Creation
+	app.get('/course_creation', function(req, res, next) {
+		res.render('course_creation', { title: 'Creating/Editing Course' });
+	});
+
+// POST for Course Creation
+	app.post('/course_creation', function(req, res, next) {
+		// Retrieve Information
+		var cid  = req.body.cid;
+		var quota = req.body.quota;
+		var name    = req.body.name;
+		var credit = req.body.credit;
+		var c_admin = req.body.c_admin;
+
+		pool.query(sql_query.query.create_course, [cid, quota, name, credit, c_admin], (err, data) => {
+			res.redirect('/courses')
+		});
+	});
+
+// GET for Courses
+	app.get('/courses', function(req, res, next) {
+		pool.query(sql_query.query.view_course, (err, data) => {
+			res.render('courses', { title: 'View Courses', data: data.rows });
+		});
+	});
+	app.get('/courses/delete/:cid', function(req, res, next) {
+		res.render('courses', { title: 'View Courses', data: data.rows });
+	});
+
+	app.post('/courses/delete/<%= data[i].cid%>', function(req,res,next) {
+		pool.query(sql_query.query.delete_course, [req.params.cid], (err,data) => {
+			res.render('courses', {title: 'View Courses', data: data.rows});
+		});
+	});
+
+// GET for Prereq Creation
+	app.get('/prereq_creation', function(req, res, next) {
+		res.render('prereq_creation', { title: 'Creating/Editing Prerequisites' });
+	});
+
+// POST for Prereq Creation
+	app.post('/prereq_creation', function(req, res, next) {
+		// Retrieve Information
+		var required_cid  = req.body.required_cid;
+		var requiring_cid  = req.body.requiring_cid;
+		var setter = req.body.setter;
+
+		pool.query(sql_query.query.create_prereq, [required_cid, requiring_cid, setter], (err, data) => {
+			res.redirect('/prereq')
+		});
+	});
+
+// GET for Prereq
+	app.get('/prereq', function(req, res, next) {
+		pool.query(sql_query.query.view_prereq, (err, data) => {
+			res.render('prereq', { title: 'View Prerequisites', data: data.rows });
+		});
+	});
+
+// GET for Student Creation
+	app.get('/student_creation', function(req, res, next) {
+		res.render('student_creation', { title: 'Creating/Editing Students' });
+	});
+
+// POST for Student Creation
+	app.post('/student_creation', function(req, res, next) {
+		// Retrieve Information
+		var sid  = req.body.sid;
+		var e_year = req.body.e_year;
+		var dname1 = req.body.dname1;
+		var dname2 = req.body.dname2;
+
+		pool.query(sql_query.query.add_students, [sid, e_year, dname1, dname2], (err, data) => {
+			res.redirect('/student')
+		});
+	});
+
+// GET for Student
+	app.get('/student', function(req, res, next) {
+		pool.query(sql_query.query.view_student, (err, data) => {
+			res.render('student', { title: 'View Students', data: data.rows });
+		});
+	});
+
+// GET for Admin Creation
+	app.get('/admin_creation', function(req, res, next) {
+		res.render('admin_creation', { title: 'Creating/Editing Administrators' });
+	});
+
+// POST for Admin Creation
+	app.post('/admin_creation', function(req, res, next) {
+		// Retrieve Information
+		var aid  = req.body.aid;
+
+		pool.query(sql_query.query.add_admin, [aid], (err, data) => {
+			res.redirect('/admin')
+		});
+	});
+
+// GET for Admin
+	app.get('/admin', function(req, res, next) {
+		pool.query(sql_query.query.view_admin, (err, data) => {
+			res.render('admin', { title: 'View Administrators', data: data.rows });
+		});
+	});
+
+
 }
 
 
--- sql/index.js
+++ sql/index.js
@@ -2,7 +2,20 @@ const sql = {}
 
 sql.query = {
 	find_user: 'SELECT * FROM USERS',
-	add_user: 'INSERT INTO users (uid, name, password) VALUES($1, $2, $3)'
+	add_user: 'INSERT INTO users (uid, name, password) VALUES($1, $2, $3)',
+
+	create_course:'INSERT INTO Courses (cid, quota, name, credit, c_admin) VALUES($1,$2,$3,$4,$5) ON CONFLICT(cid) DO UPDATE SET cid = $1, quota = $2, name = $3, credit = $4, c_admin = $5',
+	view_course: 'SELECT * FROM Courses',
+	delete_course: 'DELETE FROM Courses WHERE cid=?',
+
+	create_prereq: 'INSERT INTO Prerequisites (required_cid, requiring_cid, setter) VALUES($1,$2,$3) ON CONFLICT(required_cid,requiring_cid) DO UPDATE SET required_cid = $1, requiring_cid = $2, setter = $3',
+	view_prereq: 'SELECT * FROM Prerequisites',
+
+	create_student: 'INSERT INTO EnrolledStudents(sid, e_year, dname1, dname2) VALUES($1,$2,$3,$4) ON CONFLICT(sid) DO UPDATE SET sid = $1, e_year = $2, dname1 = $3, dname2 = $4',
+	view_student: 'SELECT * FROM EnrolledStudents',
+
+	create_admin: 'INSERT INTO Administrators(aid) VALUES($1) ON CONFLICT(aid) DO UPDATE SET aid = $1',
+	view_admin: 'SELECT * FROM Administrators'
 }
 
 module.exports = sql
