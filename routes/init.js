59f129af61abe63423b84024538a55d17aa6e0c1
Jason Tan <jason@Jasons-MacBook-Pro.local>
2019-11-05 12:04:47 +0800

    Implement allocate modules

--- routes/init.js
+++ routes/init.js
@@ -7,13 +7,26 @@ const pool = new Pool({
   //ssl: true
 });
 
-// Global Variable
+// Global Variables
 var sess = {};
+var acad_year = 2019;
+var semester = 1;
+var reg_round = 3;
 
 function initRouter(app) {
 	/* GET */
   app.get('/', function(req, res, next) {
-		    res.render('index', { title: 'ModReg',});
+			pool.query(sql_query.query.get_period, (err, data) => {
+				if (data.rows[0] == null) {
+					res.redirect('/closed');
+				} else {
+					acad_year = data.rows[0].a_year;
+					semester =  data.rows[0].semester;
+					reg_round = data.rows[0].a_year;
+				}
+		});
+		  res.render('index', { title: 'RegMod', a_year: acad_year,
+			semester: semester, round: reg_round});
 	});
 
   /* All select operations can use this template */
@@ -23,113 +36,160 @@ function initRouter(app) {
   	});
   });
 
-/* All insert operations can use this template */
-app.get('/test_insert', function(req, res, next) {
-	res.render('test_insert', { title: 'Insert' });
-});
+	/* All insert operations can use this template */
+	app.get('/test_insert', function(req, res, next) {
+		res.render('test_insert', { title: 'Insert' });
+	});
 
-//  have to add test_insert to the ejs form
-app.post('/test_insert', function(req, res, next) {
-	// Retrieve Information
-	var uid  = req.body.uid;
-	var name    = req.body.name;
-	var password = req.body.password;
+	//  have to add test_insert to the ejs form
+	app.post('/test_insert', function(req, res, next) {
+		// Retrieve Information
+		var uid  = req.body.uid;
+		var name    = req.body.name;
+		var password = req.body.password;
 
-  pool.query(sql_query.query.add_user, [uid, name, password], (err, data) => {
-    if(err) {
-      console.error("Error in adding user");
-      res.redirect('/test_insert');
-    } else {
-      res.redirect('/test_select');
-    }
-  });
-});
+	  pool.query(sql_query.query.add_user, [uid, name, password], (err, data) => {
+	    if(err) {
+	      console.error("Error in adding user");
+	      res.redirect('/test_insert');
+	    } else {
+	      res.redirect('/test_select');
+	    }
+	  });
+	});
 
   // add all app.get app.post things here
 
 
-// GET for login
-app.get('/login', function(req, res, next) {
-	if (sess["uid"] != null) {
-		res.redirect('/about')
-	} else {
-		res.render('login', { title: 'Login' });
-	}
-});
-
-// POST for login
-app.post('/login', function(req, res, next) {
-	var uid = req.body.uid;
-	var password = req.body.password;
-
-	pool.query('SELECT "password" FROM Users WHERE "uid" = $1', [uid], (err, result) => {
-		if (err) {
-			res.redirect('/relog')
-		} else if (result.rows[0] == null) {
-			res.redirect('/relog')
+	// GET for login
+	app.get('/login', function(req, res, next) {
+		if (sess["uid"] != null) {
+			res.redirect('/about')
 		} else {
-			if (result.rows[0].password == [password]) {
-				pool.query('SELECT "aid" FROM Administrator WHERE "aid" = $1', [uid], (err, result1) => {
-					sess = req.body;
-					sess["uid"] = uid;
-					if (result1.rows[0] == null) {
-						res.redirect('/about')
-					} else {
-						res.redirect('/index')
-					}
-				});
-			} else {
-				res.redirect('/relog')
-			}
+			res.render('login', { title: 'Login' });
 		}
 	});
-});
 
-// Get for About
-app.get('/about', function(req, res, next) {
- try {
-	if (sess.uid != null) {
-		res.render('about', { title: 'About' });
-	}
- } 
- catch(err) {
-	res.redirect('/login')
- }
-});
+	// POST for login
+	app.post('/login', function(req, res, next) {
+		var uid = req.body.uid;
+		var password = req.body.password;
 
-// GET for Relog
-app.get('/relog', function(req, res, next) {
-	res.render('relog', { title: 'Login' });
-});
-
-// POST for Relog
-app.post('/', function(req, res, next) {
-	var uid = req.body.uid;
-	var password = req.body.password;
-
-	pool.query('SELECT "password" FROM Users WHERE "uid" = $1', [uid], (err, result) => {
-		if (err) {
-			res.redirect('/relog')
-		} else if (result.rows[0] == null) {
-			res.redirect('/relog')
-		} else {
-			if (result.rows[0].password == [password]) {
-				pool.query('SELECT "aid" FROM Administrator WHERE "aid" = $1', [uid], (err, result1) => {
-					sess = req.body;
-					sess.uid = uid;
-					if (result1.rows[0] == null) {
-						res.redirect('/about')
-					} else {
-						res.redirect('/index')
-					}
-				});
-			} else {
+		pool.query('SELECT "password" FROM Users WHERE "uid" = $1', [uid], (err, result) => {
+			if (err) {
 				res.redirect('/relog')
+			} else if (result.rows[0] == null) {
+				res.redirect('/relog')
+			} else {
+				if (result.rows[0].password == [password]) {
+					pool.query('SELECT "aid" FROM Administrator WHERE "aid" = $1', [uid], (err, result1) => {
+						sess = req.body;
+						sess["uid"] = uid;
+						if (result1.rows[0] == null) {
+							res.redirect('/about')
+						} else {
+							res.redirect('/index')
+						}
+					});
+				} else {
+					res.redirect('/relog')
+				}
 			}
-		}
+		});
 	});
-});
-}
+
+	// Get for About
+	app.get('/about', function(req, res, next) {
+	 try {
+		if (sess.uid != null) {
+			res.render('about', { title: 'About' });
+		}
+	 }
+	 catch(err) {
+		res.redirect('/login')
+	 }
+	});
+
+	// GET for Relog
+	app.get('/relog', function(req, res, next) {
+		res.render('relog', { title: 'Login' });
+	});
+
+	// POST for Relog
+	app.post('/', function(req, res, next) {
+		var uid = req.body.uid;
+		var password = req.body.password;
+
+		pool.query('SELECT "password" FROM Users WHERE "uid" = $1', [uid], (err, result) => {
+			if (err) {
+				res.redirect('/relog')
+			} else if (result.rows[0] == null) {
+				res.redirect('/relog')
+			} else {
+				if (result.rows[0].password == [password]) {
+					pool.query('SELECT "aid" FROM Administrator WHERE "aid" = $1', [uid], (err, result1) => {
+						sess = req.body;
+						sess.uid = uid;
+						if (result1.rows[0] == null) {
+							res.redirect('/about')
+						} else {
+							res.redirect('/index')
+						}
+					});
+				} else {
+					res.redirect('/relog')
+				}
+			}
+		});
+	});
+
+	app.get('/admin_allocate_search', function(req, res, next) {
+			res.render('admin_allocate_search', { title: 'Allocated Students' });
+	});
+
+
+	app.post('/admin_allocate_search', function(req, res, next) {
+		pool.query(sql_query.query.allocated_students,[req.body.cid, acad_year,semester],
+			(err, data) => {
+				res.render('admin_allocate_select', { title: req.body.cid,
+					data: data.rows });
+			});
+	});
+
+	app.get('/admin_allocate_select', function(req, res, next) {
+		pool.query(sql_query.query.allocated_students,[req.query.cid, acad_year,semester],
+			(err, data) => {
+				res.render('admin_allocate_select', { title: req.query.cid,
+					data: data.rows });
+			});
+	});
+
+	app.post('/delete_allocate', function(req, res, next) {
+		pool.query(sql_query.query.delete_allocated_students,[req.query.cid,
+			acad_year,semester],
+			(err, data) => {
+				res.redirect('/admin_allocate_select')
+			});
+	});
+
+	app.get('/admin_allocate_insert', function(req, res, next) {
+		res.render('admin_allocate_insert');
+	});
+
+	app.post('/admin_allocate_insert', function(req, res, next) {
+		var sid  = req.body.sid;
+		var cid    = req.body.cid;
+
+	  pool.query(sql_query.query.add_user, [uid, name, password], (err, data) => {
+	    if(err) {
+	      console.error("Error in adding user");
+	      res.redirect('/admin_allocate_insert');
+	    } else {
+	      res.redirect('/admin_allocate_select?cid=' + cid);
+			}
+		});
+	});
+};
 
 
 
--- sql/index.js
+++ sql/index.js
@@ -2,7 +2,15 @@ const sql = {}
 
 sql.query = {
 	find_user: 'SELECT * FROM USERS',
-	add_user: 'INSERT INTO users (uid, name, password) VALUES($1, $2, $3)'
+	find_student: 'SELECT * FROM EnrolledStudents JOIN Users on sid = uid WHERE sid = $1',
+
+	add_user: 'INSERT INTO users (uid, name, password) VALUES($1, $2, $3)',
+	get_period: 'SELECT * FROM RegisterPeriods WHERE NOW() > s_time and NOW() < e_time;',
+	allocated_students: 'SELECT * FROM Accept NATURAL JOIN EnrolledStudents'
+	+ ' JOIN Users on sid = uid WHERE cid = $1 AND a_year = $2 AND semester = $3;',
+	delete_allocated_students: 'DELETE FROM Accept WHERE cid = $1 sid = $2;',
+
+
 }
 
 module.exports = sql
