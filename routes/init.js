4dc49f61a3e7cb18621b676ab152a35f3f83c82e
jtankw3 <j.tankw3@gmail.com>
2019-11-05 15:18:31 +0800

    Add admin module

--- routes/init.js
+++ routes/init.js
@@ -9,9 +9,9 @@ const pool = new Pool({
 
 // Global Variables
 var sess = {};
-var acad_year = 2019;
-var semester = 1;
-var reg_round = 3;
+var acad_year;
+var semester;
+var reg_round;
 
 function initRouter(app) {
 	/* GET */
@@ -64,7 +64,7 @@ function initRouter(app) {
 	// GET for login
 	app.get('/login', function(req, res, next) {
 		if (sess["uid"] != null) {
-			res.redirect('/about')
+			res.render('/about')
 		} else {
 			res.render('login', { title: 'Login' });
 		}
@@ -82,13 +82,13 @@ function initRouter(app) {
 				res.redirect('/relog')
 			} else {
 				if (result.rows[0].password == [password]) {
-					pool.query('SELECT "aid" FROM Administrator WHERE "aid" = $1', [uid], (err, result1) => {
+					pool.query('SELECT "aid" FROM Administrators WHERE "aid" = $1', [uid], (err, result1) => {
 						sess = req.body;
 						sess["uid"] = uid;
 						if (result1.rows[0] == null) {
-							res.redirect('/about')
+							res.redirect('/student_homepage')
 						} else {
-							res.redirect('/index')
+							res.redirect('/admin_homepage')
 						}
 					});
 				} else {
@@ -172,6 +172,13 @@ function initRouter(app) {
 			});
 	});
 
+	app.post('/delete_admin', function(req, res, next) {
+		pool.query(sql_query.query.delete_admin,[req.query.aid],
+			(err, data) => {
+				res.redirect('/admin')
+			});
+	});
+
 	app.get('/admin_allocate_insert', function(req, res, next) {
 		res.render('admin_allocate_insert');
 	});
@@ -189,8 +196,184 @@ function initRouter(app) {
 			}
 		});
 	});
-};
 
+// GET for Admin
+app.get('/admin_homepage', function(req, res, next) {
+	res.render('admin_homepage', { title: 'Admin Homepage' });
+});
+
+//GET for Student
+app.get('/student_homepage', function(req, res, next){
+	res.render('student_homepage', { title: 'Student Homepage' });
+});
+
+// GET for Course Creation
+	app.get('/course_creation', function(req, res, next) {
+		res.render('course_creation', { title: 'Creating/Editing Course' });
+	});
+
+// POST for Course Creation
+	app.post('/course_creation', function(req, res, next) {
+		// Retrieve Information
+		var cid  = req.body.cid;
+		var quota = req.body.quota;
+		var name    = req.body.name;
+		var credit = req.body.credit;
+		var c_admin = req.body.c_admin;
+
+		pool.query(sql_query.query.create_course, [cid, quota, name, credit, c_admin], (err, data) => {
+			res.redirect('/courses')
+		});
+	});
+
+// GET for Courses
+	app.get('/courses', function(req, res, next) {
+		pool.query(sql_query.query.view_course, (err, data) => {
+			res.render('courses', { title: 'View Courses', data: data.rows });
+		});
+	});
+	app.get('/courses/delete/:cid', function(req, res, next) {
+		res.render('courses', { title: 'View Courses', data: data.rows });
+	});
+
+	app.post('/courses/delete/<%= data[i].cid%>', function(req,res,next) {
+		pool.query(sql_query.query.delete_course, [req.params.cid], (err,data) => {
+			res.render('courses', {title: 'View Courses', data: data.rows});
+		});
+	});
+
+// GET for Prereq Creation
+	app.get('/prereq_creation', function(req, res, next) {
+		res.render('prereq_creation', { title: 'Creating/Editing Prerequisites' });
+	});
+
+// POST for Prereq Creation
+	app.post('/prereq_creation', function(req, res, next) {
+		// Retrieve Information
+		var required_cid  = req.body.required_cid;
+		var requiring_cid  = req.body.requiring_cid;
+		var setter = req.body.setter;
+
+		pool.query(sql_query.query.create_prereq, [required_cid, requiring_cid, setter], (err, data) => {
+			res.redirect('/prereq')
+		});
+	});
+
+// GET for Prereq
+	app.get('/prereq', function(req, res, next) {
+		pool.query(sql_query.query.view_prereq, (err, data) => {
+			res.render('prereq', { title: 'View Prerequisites', data: data.rows });
+		});
+	});
+
+// GET for Student Creation
+	app.get('/student_creation', function(req, res, next) {
+		res.render('student_creation', { title: 'Creating/Editing Students' });
+	});
+
+// POST for Student Creation
+	app.post('/student_creation', function(req, res, next) {
+		// Retrieve Information
+		var sid  = req.body.sid;
+		var e_year = req.body.e_year;
+		var dname1 = req.body.dname1;
+		var dname2 = req.body.dname2;
+
+		pool.query(sql_query.query.create_student, [sid, e_year, dname1, dname2], (err, data) => {
+			res.redirect('/student')
+		});
+	});
+
+// GET for Student
+	app.get('/student', function(req, res, next) {
+		pool.query(sql_query.query.view_student, (err, data) => {
+			res.render('student', { title: 'View Students', data: data.rows });
+		});
+	});
+
+// GET for Admin Creation
+	app.get('/admin_creation', function(req, res, next) {
+		res.render('admin_creation', { title: 'Creating/Editing Administrators' });
+	});
+
+// POST for Admin Creation
+	app.post('/admin_creation', function(req, res, next) {
+		// Retrieve Information
+		var aid  = req.body.aid;
+
+		pool.query(sql_query.query.add_admin, [aid], (err, data) => {
+			res.redirect('/admin')
+		});
+	});
+
+// GET for Admin
+	app.get('/admin', function(req, res, next) {
+		pool.query(sql_query.query.view_admin, (err, data) => {
+			res.render('admin', { title: 'View Administrators', data: data.rows });
+		});
+	});
+
+// GET for course_registration
+	app.get('/course_registration', function(req, res, next) {
+		res.render('course_registration', { title: 'Course Registration' });
+	});
+
+// POST for course_registration
+	app.post('/course_registration', function(req, res, next) {
+		// Retrieve Information
+		var cid  = req.body.cid;
+		var sid =  sess["uid"];
+
+		var insert_query = "INSERT INTO register VALUES('" + sid + "','" + cid + "')";
+		pool.query(insert_query, (err, data) => {
+			res.redirect('/student_homepage')
+		});
+	});
+
+	// GET for View degree requirements
+	var degree = 'Computer Science'
+	var sql_query2 = "SELECT required_cid, type FROM requirements WHERE name = '" + degree + "'";
+
+	app.get('/degree_requirements', function(req, res, next) {
+		pool.query(sql_query2, (err, data) => {
+			res.render('degree_requirements', { title: 'Degree Requirements', data: data.rows });
+		});
+	});
+
+// GET for drop course
+	app.get('/drop_course', function(req, res, next) {
+		res.render('drop_course', { title: 'Drop Course' });
+	});
+
+// POST for drop course
+	var sql_query1 = 'DELETE FROM accept WHERE cid =';
+	app.post('/drop_course', function(req, res, next) {
+		// Retrieve Information
+		var cid  = req.body.cid;
+		var sid = sess.uid;
+		// var sid = 'A00000001D'
+
+		// Construct Specific SQL Query
+		var drop_query = sql_query1 + "'" + cid + "'" + "AND sid ='" + sid + "'";
+
+		pool.query(drop_query, (err, data) => {
+			res.redirect('/student_homepage')
+		});
+	});
+
+	// GET for view course
+	app.get('/view_course', function(req, res, next) {
+		var sid = sess.uid;
+		pool.query('SELECT A.cid, C.name FROM Accept A JOIN Courses C ON A.cid = C.cid WHERE A.sid =' + "'" + sid + "'", (err, data) => {
+			res.render('view_course', { title: 'View Courses', data: data.rows });
+		});
+	});
+
+	app.get('/logout', function(req, res, next) {
+			sess["uid"] = null
+			res.render('login', { title: 'Login' });
+	});
+};
 
 
 
--- sql/index.js
+++ sql/index.js
@@ -1,17 +1,37 @@
+/*const sql = {}
+
+sql.query = {
+	find_user: 'SELECT * FROM USERS',
+	add_user: 'INSERT INTO users (uid, name, password) VALUES($1, $2, $3)',
+	course_add:'INSERT INTO register (sid, cid) VALUES($1, $2)'
+}
+
+module.exports = sql*/
+
 const sql = {}
 
 sql.query = {
 	find_user: 'SELECT * FROM USERS',
 	find_student: 'SELECT * FROM EnrolledStudents JOIN Users on sid = uid WHERE sid = $1',
-
 	add_user: 'INSERT INTO users (uid, name, password) VALUES($1, $2, $3)',
 	get_period: 'SELECT * FROM RegisterPeriods WHERE NOW() > s_time and NOW() < e_time;',
 	allocated_students: 'SELECT * FROM Accept NATURAL JOIN EnrolledStudents'
-	+ ' JOIN Users on sid = uid WHERE cid = $1 AND a_year = $2 AND semester = $3;',
-	delete_allocated_students: 'DELETE FROM Accept WHERE cid = $1 sid = $2;',
-	insert_allocated_students: 'INSERT INTO Accept VALUES();',
+	+ ' JOIN Users on sid = uid WHERE cid = $1 AND a_year = $2 AND semester = $3',
+	delete_allocated_students: 'DELETE FROM Accept WHERE cid = $1 sid = $2',
 
+	create_course:'INSERT INTO Courses (cid, quota, name, credit, c_admin) VALUES($1,$2,$3,$4,$5) ON CONFLICT(cid) DO UPDATE SET cid = $1, quota = $2, name = $3, credit = $4, c_admin = $5',
+	view_course: 'SELECT * FROM Courses',
+	delete_course: 'DELETE FROM Courses WHERE cid=?',
 
+	create_prereq: 'INSERT INTO Prerequisites (required_cid, requiring_cid, setter) VALUES($1,$2,$3) ON CONFLICT(required_cid,requiring_cid) DO UPDATE SET required_cid = $1, requiring_cid = $2, setter = $3',
+	view_prereq: 'SELECT * FROM Prerequisites',
+
+	create_student: 'INSERT INTO EnrolledStudents(sid, e_year, dname1, dname2) VALUES($1,$2,$3,$4) ON CONFLICT(sid) DO UPDATE SET sid = $1, e_year = $2, dname1 = $3, dname2 = $4',
+	view_student: 'SELECT * FROM EnrolledStudents',
+
+	create_admin: 'INSERT INTO Administrators(aid) VALUES($1) ON CONFLICT(aid) DO UPDATE SET aid = $1',
+	view_admin: 'SELECT * FROM Administrators',
+	edit_admin: 'DELETE FROM Accept WHERE cid = $1 sid = $2 CASCADE'
 }
 
 module.exports = sql
